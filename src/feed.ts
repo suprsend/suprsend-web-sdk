e60e7545538c1bf38991f5a531ac6298aa339cb1
Sivaram <89124860+sivaram000@users.noreply.github.com>
2025-10-12 21:34:33 +0530

    bugfix: socket connection authentication failed issue after refresh (#23)

--- src/feed.ts
+++ src/feed.ts
@@ -1,5 +1,6 @@
 import { createStore, StoreApi } from 'zustand/vanilla';
 import { io, Socket } from 'socket.io-client';
+import jwt_decode from 'jwt-decode';
 import mitt, { Emitter } from 'mitt';
 import SuprSend from './main';
 import {
@@ -184,6 +185,42 @@ export class Feed {
   }
 
   private initializeSocketEvents() {
+    this.socket.on('connect_error', async (error) => {
+      if (
+        error.message === 'Authentication Error: wrong auth token' &&
+        this.config.authenticateOptions?.refreshUserToken &&
+        this.config.userToken
+      ) {
+        const jwtPayload = jwt_decode(this.config.userToken) as Dictionary;
+        const expiresOn = ((jwtPayload.exp as number) || 0) * 1000; // in ms
+        const now = Date.now(); // in ms
+        const hasExpired = expiresOn <= now;
+        if (hasExpired) {
+          try {
+            const newUserToken =
+              await this.config.authenticateOptions.refreshUserToken(
+                this.config.userToken,
+                jwtPayload
+              );
+
+            if (newUserToken && typeof newUserToken === 'string') {
+              await this.config.identify(
+                this.config.distinctId,
+                newUserToken,
+                this.config.authenticateOptions
+              );
+              this.socket.auth['x-ss-signature'] = newUserToken;
+              setTimeout(() => {
+                this.socket.connect();
+              }, 1000);
+            }
+          } catch (e) {
+            // error while getting token go ahead with calling api
+          }
+        }
+      }
+    });
+
     this.socket.on(
       'new_notification',
       this.handleNewNotificationSocketEvent.bind(this)
