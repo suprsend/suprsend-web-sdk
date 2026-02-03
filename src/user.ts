8ac2a4f2b9e3af716d4b63946f12acb98a02a40b
Hamzaisongit <hamzaravani0@gmail.com>
2026-02-03 17:54:10 +0530

    fix(user.removeWebpush): unsubscribe the push service before making internal remove call

--- src/user.ts
+++ src/user.ts
@@ -466,7 +466,7 @@ export default class User {
     }
 
     const deviceId: string = this.getDeviceId();
-
+    
     return this.appendInternal({
       $webpush: push,
       $id_provider: 'vapid',
@@ -485,7 +485,9 @@ export default class User {
     }
 
     const deviceId: string = this.getDeviceId();
-
+    
+    await push.unsubscribe();
+    
     return this.removeInternal({
       $webpush: push,
       $id_provider: 'vapid',
